3097ebd2a2b573ad1eb3c1f3ae14e81a539630af
li-hao <li-hao@meyougames.com>
2013-04-08 12:03:00 +0800

    重写sprite的draw方法
    Signed-off-by: li-hao <li-hao@meyougames.com>

---
+++ gitdemo3/CustomSprite.cpp
@@ -0,0 +1,9 @@
+//
+//  CustomSprite.cpp
+//  gitdemo3
+//
+//  Created by MeYouGamesP2 on 13-4-8.
+//
+//
+
+#include "CustomSprite.h"
---
+++ gitdemo3/CustomSprite.h
@@ -0,0 +1,14 @@
+//
+//  CustomSprite.h
+//  gitdemo3
+//
+//  Created by MeYouGamesP2 on 13-4-8.
+//
+//
+
+#ifndef __gitdemo3__CustomSprite__
+#define __gitdemo3__CustomSprite__
+
+#include <iostream>
+
+#endif /* defined(__gitdemo3__CustomSprite__) */

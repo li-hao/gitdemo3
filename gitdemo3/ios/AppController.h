e7b223a61807a5a8762e16f831ae93b9a824a74d
li-hao <li-hao@meyougames.com>
2013-04-08 10:15:41 +0800

    Initial Commit

---
+++ gitdemo3/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  gitdemo3AppController.h
+//  gitdemo3
+//
+//  Created by MeYouGamesP2 on 13-4-8.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ gitdemo3/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  gitdemo3AppController.h
+//  gitdemo3
+//
+//  Created by MeYouGamesP2 on 13-4-8.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
